46314db427fff499ca5e0d3a759d51dd5e85668c
at//design <mail@at-world.de>
2024-03-31 18:11:15 +0200

    Added strict concurrency

--- Package.swift
+++ Package.swift
@@ -4,21 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "NGCollectionHelpers",
-    platforms: [.iOS(.v17), .macOS(.v14), .visionOS(.v1), .watchOS(.v10), .tvOS(.v17)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NGCollectionHelpers",
-            targets: ["NGCollectionHelpers"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NGCollectionHelpers"),
-        .testTarget(
-            name: "NGCollectionHelpersTests",
-            dependencies: ["NGCollectionHelpers"]),
-    ]
+  name: "NGCollectionHelpers",
+  platforms: [.iOS(.v17), .macOS(.v14), .visionOS(.v1), .watchOS(.v10), .tvOS(.v17)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "NGCollectionHelpers",
+      targets: ["NGCollectionHelpers"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "NGCollectionHelpers",
+      swiftSettings: [
+        .enableExperimentalFeature("StrictConcurrency")
+      ]
+    ),
+    .testTarget(
+      name: "NGCollectionHelpersTests",
+      dependencies: ["NGCollectionHelpers"]),
+  ]
 )
